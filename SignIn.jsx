fc796b81eba809e839db7642493a3269570c6e66
Gopinadh Banothu <banothugopinadh09@gmail.com>
2025-02-13 15:06:16 +0530

    Add files via upload

---
+++ SignIn.jsx
@@ -0,0 +1,116 @@
+// Helps in understanding MUI components
+// Design of signIn page
+// Author: www.mrnbt.in
+// Refer Material UI components https://mui.com/material-ui/all-components/
+
+import React from "react";
+import { TextField } from "@mui/material";
+import { Button } from "@mui/material";
+import FormLabel from "@mui/material/FormLabel";
+import FormControl from "@mui/material/FormControl";
+import { FormControlLabel } from "@mui/material";
+import Link from "@mui/material/Link";
+import { Checkbox } from "@mui/material";
+
+function SignIn() {
+  function handleClick() {
+    alert("Button clicked!");
+  }
+  return (
+    <div className="align">
+      <div className="signIn-container">
+        <h1>
+          <b>Sign Up</b>
+        </h1>
+        <p class="sub-heading">please fill in this form</p>
+
+        <TextField
+          id="filled-basic"
+          label="first name"
+          variant="filled"
+        ></TextField>
+        <TextField
+          id="filled-basic"
+          label="last name"
+          variant="filled"
+        ></TextField>
+        <TextField id="filled-basic" label="Email" variant="filled"></TextField>
+        <TextField
+          id="filled-basic"
+          label="Password"
+          variant="filled"
+        ></TextField>
+        <TextField
+          id="filled-basic"
+          label="Confirm Password"
+          variant="filled"
+        ></TextField>
+        <Button variant="contained" onClick={handleClick}>Sign In</Button>
+      </div>
+    </div>
+  );
+}
+// function SignIn() {
+//   const validateInputs = () => {};
+
+//   const handleClickOpen = () => {};
+
+//   return (
+//     <div>
+//         <Box>
+//       <FormControl>
+//         <FormLabel htmlFor="email">Email</FormLabel>
+//         <TextField
+//           id="email"
+//           type="email"
+//           name="email"
+//           placeholder="your@email.com"
+//           autoComplete="email"
+//           autoFocus
+//           required
+//           fullWidth
+//           variant="outlined"
+//         />
+//       </FormControl>
+//       <FormControl>
+//         <FormLabel htmlFor="password">Password</FormLabel>
+//         <TextField
+//           name="password"
+//           placeholder="••••••"
+//           type="password"
+//           id="password"
+//           autoComplete="current-password"
+//           autoFocus
+//           required
+//           fullWidth
+//           variant="outlined"
+//         />
+//       </FormControl>
+//       <FormControlLabel
+//         control={<Checkbox value="remember" color="primary" />}
+//         label="Remember me"
+//       />
+
+//       <Button
+//         type="submit"
+//         fullWidth
+//         variant="contained"
+//         onClick={validateInputs}
+//       >
+//         Sign in
+//       </Button>
+//       <Link
+//         component="button"
+//         type="button"
+//         onClick={handleClickOpen}
+//         variant="body2"
+//         sx={{ alignSelf: "center" }}
+//       >
+//         Forgot your password?
+//       </Link>
+//       </Box>
+//     </div>
+//   );
+// }
+
+export default SignIn;
